c789663407ef94e9459602ce02523980011f695d
test1no1 <113826306+test1no1@users.noreply.github.com>
2022-09-20 18:00:12 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,10 @@
-password.onblur= function() {
-    var password = document.getElementById("password"); //得到账户的对象
-    var username = document.getElementById("username"); //得到用户所输入的账户
-    if(password.value.length < 6 ||password.value.length  > 16){ alert("你输入的密码长度不符合要求")}
-    else if(password.value == "testno1"&& username.value == "test"){ alert("登入成功")}
-    else if(username.value != "liujiayi"){alert("用户名错误！")}
-    else{ alert("密码错误!!!")}
-} 
+/*密碼檢驗程式*/
+
+var password = testno1;
+var input;
+
+while (password!=input){
+input=prompt("請輸入貓友名稱及密碼");
+}
+
+alert("成功登入");
